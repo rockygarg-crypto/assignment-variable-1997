5d4fc53ff743df225cdde7f40294d0eb71b10521
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-02 22:29:27 +0530

    its rocky

---
+++ 01-swapping-number.py
@@ -0,0 +1,12 @@
+n1=20
+n2=30
+print(f"before swapping value of n1 : {n1}")
+print(f"before swapping value of n2 : {n2}")
+
+x=n1
+n1=n2
+n2=x
+print("-"*40)
+print(f"after swapping value of n1 : {n1}")
+print(f"after swapping value of n2 : {n2}")
+
