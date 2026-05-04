d34f26e83468f1538d2829d56da5f7376f2abdba
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-04 13:49:02 +0530

    05-bank-after-withd.py

---
+++ 01-assignment-variable/05-bank-after-withd.py
@@ -0,0 +1,7 @@
+before_balance = 12000
+withdrawl = 3000
+print(f"before_balance : {before balance}")
+print(f"withdrawl : {withdrawl}")
+
+after wthdrawl = before_balance - withdrawl
+print(f"after_withdrawl : {after_withdrawl}")
