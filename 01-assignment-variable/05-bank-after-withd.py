94eb52c5b73afe70a8a667d6446150c8ecb89fa6
rockygarg-crypto <rockygarg74@gmail.com>
2026-05-04 14:12:05 +0530

    bank withdrawl

--- 01-assignment-variable/05-bank-after-withd.py
+++ 01-assignment-variable/05-bank-after-withd.py
@@ -1,7 +1,7 @@
 before_balance = 12000
 withdrawl = 3000
-print(f"before_balance : {before balance}")
+print(f"before_balance : {before_balance}")
 print(f"withdrawl : {withdrawl}")
 
-after wthdrawl = before_balance - withdrawl
-print(f"after_withdrawl : {after_withdrawl}")
+after_wthdrawl = before_balance - withdrawl
+print(f"after withdrawl : {after_wthdrawl}")
